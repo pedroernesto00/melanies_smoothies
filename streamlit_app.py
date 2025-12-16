f60ae1e083bf8cdbf3c914aa135bb488db6dc993
pedroernesto00 <39915443+pedroernesto00@users.noreply.github.com>
2025-12-16 16:13:31 +0100

    Update streamlit_app.py
    Test connexion with Smoothie API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -36,3 +37,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
